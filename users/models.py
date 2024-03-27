e754ca7a8ef59876ebf075359e7ab95cb5fd7470
dahdo <dndashimye15@gmail.com>
2024-03-27 02:23:12 +0100

    implement Address model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from phonenumber_field.modelfields import PhoneNumberField
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class Address(models.Model):
+    country = CountryField()
+    city = models.CharField(max_length=100, blank=False, null=False)
+    street = models.CharField(max_length=100, blank=False, null=False)
+    apartment = models.CharField(max_length=100, blank=True, null=True)
+    postal_code = models.CharField(max_length=20, blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return self.user.get_full_name()
+

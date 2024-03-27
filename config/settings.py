ed1220eb29d4efa53f52ea47f8df9ba249cdafc8
dahdo <dndashimye15@gmail.com>
2024-03-27 02:27:19 +0100

    update settings.py and urls.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    # Third party apps
+    'phonenumber_field',
+    # Local apps
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -114,3 +117,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# For meadia 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"

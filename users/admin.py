7d596450f0617e8290e68fed49592311c928b99a
dahdo <dndashimye15@gmail.com>
2024-04-01 21:09:32 +0200

    update admin and settings files

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from users.models import PhoneNumber, CustomUser, Profile
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.register(PhoneNumber)
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Profile)

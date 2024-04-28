eafe521f25631c32fa77e271a97d38be9b4b3c59
dahdo <dndashimye15@gmail.com>
2024-04-28 21:57:32 +0200

    added avatar upload via api

--- users/models.py
+++ users/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from phonenumber_field.modelfields import PhoneNumberField
+import os
 
 class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=30, blank=True, null=True)
     last_name = models.CharField(max_length=30, blank=True, null=True)
-    # phone_number = models.OneToOneField(PhoneNumber, related_name="user", on_delete=models.CASCADE, blank=True, null=True)
     phone_number = PhoneNumberField(unique=True, null=True, blank=True)
 
     # provide unique relate_name for groups and user_permissions
@@ -18,6 +18,11 @@ class CustomUser(AbstractUser):
         return f"user: @{self.username}"
 
 
+def avatar_upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{instance.user.username}.{ext}"
+    return os.path.join('avatars', filename)
+
 class Profile(models.Model):
     LANGUAGES_SPOKEN = [
         ('en', 'English'),
@@ -27,7 +32,7 @@ class Profile(models.Model):
     ]
     user = models.OneToOneField(CustomUser, related_name='profile', on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True)
-    avatar = models.ImageField(upload_to='avatar', blank=True, null=True)
+    avatar = models.ImageField(upload_to=avatar_upload_to, blank=True, null=True)
     languages_spoken = models.CharField(max_length=20, choices=LANGUAGES_SPOKEN, default='en')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -37,14 +42,6 @@ class Profile(models.Model):
     
     def __str__(self):
         return f"profile: @{self.user.username}"
-
-    @property
-    def avatar_url(self):
-        try:
-            url = self.avatar.url
-        except:
-            url = ''
-        return url
 
     # Signal to create a UserProfile instance once a CustomUser instance is created
     @receiver(post_save, sender=CustomUser)

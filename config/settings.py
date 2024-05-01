57d40e6e29b1de0ce24a5cb43a3966ed46d8b325
dahdo <dndashimye15@gmail.com>
2024-05-01 21:07:51 +0200

    disable admin

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'venue-connect-092139c4b592.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',

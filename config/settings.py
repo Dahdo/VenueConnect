7ae6cd01171cf780015f8beef0699cea7f9dc6a0
dahdo <dndashimye15@gmail.com>
2024-05-02 18:00:36 +0200

    db conf

--- config/settings.py
+++ config/settings.py
@@ -84,14 +84,23 @@ if IS_HEROKU_APP:
         ),
     }
 else:
+    # DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'venueconnect',
+    #     'USER': 'vcadmin',
+    #     'PASSWORD': 'venueconnect@2024',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'venueconnect',
-        'USER': 'vcadmin',
-        'PASSWORD': 'venueconnect@2024',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': 'dgmes10sn26bs',
+        'USER': 'qtcjyedwtcqhdk',
+        'PASSWORD': 'bfbb095427a307ac30a3f9b031cf2b6e9895378dd19b8b251af20c355b2e0e29',
+        'HOST': 'ec2-54-144-112-84.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
